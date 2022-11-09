57a9662f5e734ff452258ac297012147588bc427
ncases2022 <114923420+ncases2022@users.noreply.github.com>
2022-11-08 22:44:39 -0500

    Added rebreathed percent graphs

--- PROCESSING/CO2_Functions.R
+++ PROCESSING/CO2_Functions.R
@@ -1,6 +1,35 @@
 # See https://github.com/Edderic/breathesafe/blob/refactor/python/notebooks/nc-alliance-for-school-equity-and-safety.ipynb
 # For Basis of Calculations ...
-
+library(ggplot2);
+makeCO2_Graph <- function(d){
+  max <- ceiling((max(d$CO2)+250)/100) * 100;
+  d$Time2 <- parse_date_time(d$Time,c('%H:%M:%S %p','%m/%d/%Y %H%M',"'%H:%M:%S'"))
+  if (nrow(d) > 1){
+    graph <- ggplot(d, aes(x=Time2, y=CO2)) +
+      geom_line(size=1) +
+      scale_y_continuous(limits=c(400,max),breaks=seq(400,3200,200)) +
+      theme_bw() + xlab("Time") + ylab("CO2 (PPM)") +
+      scale_color_manual(values=rev(c("#7fc97f", "#beaed4","#fdc086"))) +
+      scale_x_datetime(breaks = scales::date_breaks("1 hour"), date_labels = "%H:%M") +
+      geom_hline(yintercept = 800, linetype="dotted", color = "gray", size=.5) + 
+      labs(caption=file
+      )
+    return(graph);
+  }
+  if (nrow(d) == 1){
+    graph <- ggplot(d, aes(x=Time, y=CO2)) +
+      geom_point(size=5) +
+      scale_y_continuous(limits=c(400,max),breaks=seq(400,3200,200)) +
+      theme_bw() + xlab("Time") + ylab("CO2 (PPM)") +
+      #scale_color_manual(values=rev(c("#7fc97f", "#beaed4","#fdc086"))) +
+      #scale_x_datetime(breaks = scales::date_breaks("1 hour"), date_labels = "%H:%M") +
+      geom_hline(yintercept = 800, linetype="dotted", color = "gray", size=.5) + 
+      labs(caption=file
+      )
+    return(graph);
+  }
+  return(NULL)
+}
 calculate_steady_state_co2 <- function(n.students, n.teachers, classroom.size.sqft, CFMPerPerson
                                        , CFMPerSQFt, outsideCO2=420, classroom.ceiling.ft = 10)
 {
@@ -22,6 +51,30 @@ calculate_steady_state_co2 <- function(n.students, n.teachers, classroom.size.sq
   print(paste0("Steady State CO2: ", round(ssCO2)))
   return(ssCO2);
 }
+percent_rebreathed <- function(indoor.CO2, ambient.CO2=420){
+  cbreath <- 38000;
+  rebreathed.pct <- (indoor.CO2-ambient.CO2)/cbreath;
+  print(paste0("Rebreathed %: ",round(rebreathed.pct*100,2)));
+  return(rebreathed.pct);
+}
+makeCO2_Rebreathed_Graph <- function(d){
+  d$rebreathed <- unlist(lapply(d$CO2,FUN=percent_rebreathed));
+  max <- max(d$rebreathed)+.01;
+  d$Time2 <- parse_date_time(d$Time,c('%H:%M:%S %p','%m/%d/%Y %H%M',"'%H:%M:%S'"))
+  if (nrow(d) > 1){
+    graph <- ggplot(d, aes(x=Time2, y=rebreathed)) +
+      geom_line(size=1) +
+      scale_y_continuous(limits=c(0,max),breaks=seq(0,1,.005),labels = scales::percent) +
+      theme_bw() + xlab("Time") + ylab("Rebreathed %") +
+      #scale_color_manual(values=rev(c("#7fc97f", "#beaed4","#fdc086"))) +
+      scale_x_datetime(breaks = scales::date_breaks("1 hour"), date_labels = "%H:%M") +
+      #geom_hline(yintercept = 800, linetype="dotted", color = "gray", size=.5) + 
+      labs(caption=file
+      )
+    return(graph);
+  }
+  return(NULL)
+}
 # Examples to match above ...
 #ssCO2 <- calculate_steady_state_co2(n.students=16
 #                           ,n.teachers=1
@@ -35,3 +88,4 @@ calculate_steady_state_co2 <- function(n.students, n.teachers, classroom.size.sq
 #                           ,CFMPerPerson=7.5
 #                           ,CFMPerSQFt=0
 #                           ,outsideCO2=420)
+#percent_rebreathed(indoor.CO2=5000)
--- make_individual_graphs.R
+++ make_individual_graphs.R
@@ -3,17 +3,20 @@
 #install.packages("ggplot2",source="binary");
 #install.packages("lubridate",source="binary");
 # these packages must load ...
+source("PROCESSING/CO2_Functions.R")
 library(devtools);
 library(ggplot2);
 library(lubridate);
 library(tools);
+
 overwrite = FALSE;
 # Open all CSV files ...  
-temp = list.files(path="~/Documents/SchoolCO2Data/SchoolCO2Data",pattern="*.csv", recursive=TRUE)
+temp = list.files(path="~/Documents/SchoolCO2Data/SchoolCO2Data",pattern="*\\.csv", recursive=TRUE)
 for (x in 1:length(temp))
 {
   file = temp[x];
   print(paste0("Opened: ",file));
+  # Write CO2 Graph ...
   graph.file <- paste0(file_path_sans_ext(file),".png");
   file_exists = file.exists(graph.file);
   if(file_exists & !overwrite){
@@ -21,33 +24,24 @@ for (x in 1:length(temp))
   }
   if(!file_exists){
     d <- read.csv(file=file);
-    names(d)
-    max <- ceiling((max(d$CO2)+250)/100) * 100;
-    d$Time2 <- parse_date_time(d$Time,c('%H:%M:%S %p','%m/%d/%Y %H%M',"'%H:%M:%S'"))
-    if (nrow(d) > 1){
-    graph <- ggplot(d, aes(x=Time2, y=CO2)) +
-      geom_line(size=1) +
-      scale_y_continuous(limits=c(400,max),breaks=seq(400,3200,200)) +
-      theme_bw() + xlab("Time") + ylab("CO2 (PPM)") +
-      scale_color_manual(values=rev(c("#7fc97f", "#beaed4","#fdc086"))) +
-      scale_x_datetime(breaks = scales::date_breaks("1 hour"), date_labels = "%H:%M") +
-      geom_hline(yintercept = 800, linetype="dotted", color = "gray", size=.5) + 
-      labs(caption=file
-      )
-    }
-    if (nrow(d) == 1){
-    graph <- ggplot(d, aes(x=Time, y=CO2)) +
-      geom_point(size=5) +
-      scale_y_continuous(limits=c(400,max),breaks=seq(400,3200,200)) +
-      theme_bw() + xlab("Time") + ylab("CO2 (PPM)") +
-      #scale_color_manual(values=rev(c("#7fc97f", "#beaed4","#fdc086"))) +
-      #scale_x_datetime(breaks = scales::date_breaks("1 hour"), date_labels = "%H:%M") +
-      geom_hline(yintercept = 800, linetype="dotted", color = "gray", size=.5) + 
-      labs(caption=file
-           )
-    }
+    graph <- makeCO2_Graph(d);
+    print(graph);
+    print(paste0("Saved: ",graph.file))
+    ggsave(graph,file=graph.file,width=6*1.3,height=3.5*1.3)
+  }
+  # Write Rebreathed Graph
+  graph.file <- paste0(file_path_sans_ext(file),"_rebeathed.png");
+  file_exists = file.exists(graph.file);
+  if(file_exists & !overwrite){
+    print(paste0("Graph found, Skipping: ",graph.file))
+  }
+  if(!file_exists){
+    d <- read.csv(file=file);
+    graph <- makeCO2_Rebreathed_Graph(d);
     print(graph);
     print(paste0("Saved: ",graph.file))
     ggsave(graph,file=graph.file,width=6*1.3,height=3.5*1.3)
   }
 }
+#g <- makeCO2_Rebreathed_Graph(d);
+#g;
